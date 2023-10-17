76ecc1689b20b7fe289400b304fbe3e96235eee2
yashPundhir <yash.pundhir.prof@gmail.com>
2023-10-17 12:17:35 +0530

    Media Query breakpoints updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			screens: {
+				xs: "480px",
+				sm: "600px",
+				md: "825px",
+				lg: "1024px",
+				xl: "1280px",
+			},
+		},
 	},
 	plugins: [],
 };
