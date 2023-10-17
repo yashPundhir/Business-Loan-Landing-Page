bad4222e9149c1641e6b2f241e404ab1df64cbd6
yashPundhir <yash.pundhir.prof@gmail.com>
2023-10-17 12:10:30 +0530

    constants utility file created

---
+++ src/utils/constants.js
@@ -0,0 +1,13 @@
+import secure from "../assets/secure2.svg";
+import time from "../assets/time.svg";
+import rupee from "../assets/rupee.svg";
+import percent from "../assets/percent.svg";
+
+const cardData = [
+	{ img: secure, data: "Safe And Secure" },
+	{ img: time, data: "2-3 Minute Process" },
+	{ img: rupee, data: "Funds Credited Within 24 Hours" },
+	{ img: percent, data: "Low Interest Rates " },
+];
+
+export default cardData;
